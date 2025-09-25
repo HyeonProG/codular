3d43c2fb6b4318d58e8de0e559a504b3ee185a91
HyeonProG <guswo3443@naver.com>
2025-09-25 16:24:24 +0900

    first commit

---
+++ src/test/java/com/codula/CodularApplicationTests.java
@@ -0,0 +1,13 @@
+package com.codula;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CodularApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
